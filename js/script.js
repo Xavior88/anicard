503fdeefcf14d425df71096d3f4647a2a977177c
Kiriyako <92046315+Kiriyako@users.noreply.github.com>
2022-12-16 17:16:15 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,16 +7,21 @@ const API_URL = 'https://api.jikan.moe/v4';
        data.data.forEach(item => {
     const anime = item;
     const imageUrl = item.images;
+    const aired = item.aired;
     const animeDataDiv = document.createElement('div');
     animeDataDiv.innerHTML = `
           <p></p>
             <img src ="${imageUrl.jpg.image_url}"><p></p>
             <b>Title:</b> <text> ${anime.title}</text><br>
             <b>Local Name:</b><text> ${anime.title_japanese}</text> <br>
+            <b>Type and Source:</b><text> ${anime.type}, ${anime.source}</text> <br>
             <b>Synopsis:</b> <text>${anime.synopsis}</text><br>
-            <b>Type:</b><text> ${anime.type}</text> <br>
+            <b>Aired:</b><text> ${aired.string}</text> <br>
             <b>Total Episodes:</b><text> ${anime.episodes}</text> <br>
-          
+            <b>Average Episode Duration:</b><text> ${anime.duration}</text> <br>
+            <b>Status:</b><text> ${anime.status}</text> <br>
+            <b>Age Rating:</b><text> ${anime.rating}</text> <br>
+
           `;
 
     document.getElementById('anime-data').appendChild(animeDataDiv);
