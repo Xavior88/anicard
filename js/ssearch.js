5aab00908c3b9bd64aea0ce99a5e0df6ef27355c
Kiriyako <92046315+Kiriyako@users.noreply.github.com>
2022-12-16 23:02:52 +0530

    Add files via upload

---
+++ js/ssearch.js
@@ -0,0 +1,39 @@
+const API_URL = 'https://api.jikan.moe/v4/seasons';
+function getAnimeData(){
+    const YEAR = document.getElementById('year').value;
+    const SEASON = document.getElementById('season').value;
+
+    fetch(`${API_URL}/${YEAR}/${SEASON}`)
+    .then(response => response.json())
+    .then(data => {
+   data.data.forEach(item => {
+const anime = item;
+const imageUrl = item.images;
+const aired = item.aired;
+const broadcast = item.broadcast;
+const animeDataDiv = document.createElement('div');
+animeDataDiv.innerHTML = `
+      <p></p>
+        <img src ="${imageUrl.jpg.image_url}"><p></p>
+        <b>Title:</b> <text> ${anime.title}</text><br>
+        <b>Japanese Title:</b><text> ${anime.title_japanese}</text> <br>
+        <b>Type and Source:</b><text> ${anime.type}, ${anime.source}</text> <br>
+        <b>Synopsis:</b> <text>${anime.synopsis}</text><br>
+        <b>Airing Timings and Status:</b><text> ${broadcast.string},  ${aired.string}</text> <br>
+        <b>Total Episodes:</b><text> ${anime.episodes}</text> <br>
+        <b>Duration:</b><text> ${anime.duration}</text> <br>
+        <b>Status:</b><text> ${anime.status}</text> <br>
+        <b>Age Rating:</b><text> ${anime.rating}</text> <br>
+
+      `;
+
+document.getElementById('anime-data').appendChild(animeDataDiv);
+})
+});
+}
+
+document.addEventListener('keydown', function(event) {
+if (event.key === 'Enter') {
+clearcontent();
+}
+});    
